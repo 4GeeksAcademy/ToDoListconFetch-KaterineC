c299c08848413fd3c5dd55679390d76147ae9225
Katerine Céspedes <katerinecesbal@hotmail.com>
2024-10-30 20:05:14 +0000

    cargar agregado

--- src/js/component/todos.jsx
+++ src/js/component/todos.jsx
@@ -1,47 +1,55 @@
-import React, {useState, useEffect} from "react";
-
+import React, { useState, useEffect } from "react";
 
 const Monday = () => {
+  let [tareas, setTareas] = useState([]);
+  let [tareaNueva, setTareaNueva] = useState("");
 
-	let [tareas, setTareas] = useState([])
-	let [tareaNueva, setTareaNueva] = useState([])
+  const cargarTareas = async () => {
+	
+    const url = "https://playground.4geeks.com/todo/users/drastone";
+    const resp = await fetch(url);
+    const data = await resp.json();
+    setTareas(data.todos);
+  };
 
+  useEffect(() => {
+    cargarTareas();
+  }, []);
 
+  return (
+    <div className="container my-5 background col-4">
+      <h1 className="text-center">Tasks</h1>
+      <div>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="What's needs to be done?"
+          value={tareaNueva}
+          onChange={(event) => {
+            setTareaNueva(event.target.value);
+          }}
+          onKeyUp={async (event) => {
+            if (event.key === "Enter") {
+              const url = "https://playground.4geeks.com/todo/todos/drastone";
+              const resp = await fetch(url, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  label: tareaNueva,
+                  is_done: false,
+                }),
+              });
+              if (resp.ok) {
+                setTareaNueva(""); 
+                cargarTareas(); 
+              }
+            }
+          }}
+        />
 
-	useEffect( () =>{
-		const cargarTareas = async () =>{
-			const url= "https://playground.4geeks.com/todo/users/drastone"
-			const resp = await fetch(url) 
-			const data = await resp.json()			
-		
-			setTareas(data.todos)
-		}
-		cargarTareas();
-	},[])
-
-
-
-	return (
-		<div className="container my-5 background col-4">
-
-			<h1 className="text-center">Tasks</h1>
-			<div>
-				<input type="text" className="form-control" placeholder="What's needs to be done?"
-					value={tareaNueva} onChange={(event) =>{
-						setTareaNueva(event.target.value)
-					}}
-
-					onKeyUp = {(event) => {
-
-						if(event.key == "Enter"){
-							setTareas([...tareas,tareaNueva ])
-							setTareaNueva("")
-						}
-
-					}}
-				/>
-			
-			<ul>
+        <ul>
           {tareas.length === 0 ? (
             <li>No hay tareas, añadir tareas</li>
           ) : (
@@ -59,10 +67,10 @@ const Monday = () => {
             ))
           )}
         </ul>
-						<span>{tareas.length} Items left</span>
-			</div>
-		</div>
-	);
+        <span>{tareas.length} Items left</span>
+      </div>
+    </div>
+  );
 };
 
 export default Monday;
