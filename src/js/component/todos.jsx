88afe5d340b7b9e24d95a55f6e5997dc56b02520
Katerine Céspedes <katerinecesbal@hotmail.com>
2024-10-31 21:46:31 +0000

    boton borrar todo

--- src/js/component/todos.jsx
+++ src/js/component/todos.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+const urlBase = "https://playground.4geeks.com/todo"
+
 const Monday = () => {
   let [tareas, setTareas] = useState([]);
   let [tareaNueva, setTareaNueva] = useState("");
@@ -7,73 +9,88 @@ const Monday = () => {
 
 
   const cargarTareas = async () => {
-		try {
-			const url = "https://playground.4geeks.com/todo/users/drastone"
-			const resp = await fetch(url)
-			if (resp.status == 404) {
-				crearUsuario()
-				return
-			}
-			const data = await resp.json()
-			setTareas(data.todos)
-			return true
-		} catch (error) {
-			console.error(error)
-			return false
-		}
-	}
+    try {
+      const url = "https://playground.4geeks.com/todo/users/drastone"
+      const resp = await fetch(url)
+      if (resp.status == 404) {
+        crearUsuario()
+        return
+      }
+      const data = await resp.json()
+      setTareas(data.todos)
+      return true
+    } catch (error) {
+      console.error(error)
+      return false
+    }
+  }
+
+
+
+
+  const borrar = async (id) => {
+
+    try {
+      const resp = await fetch("https://playground.4geeks.com/todo/todos/" + id, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" }
+      })
+
+      if (resp.status == 204) {
+        cargarTareas()
+        return true
+      }
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
+
+  const deleteUser = async () => {
+    try {
+      const resp = await fetch(`${urlBase}/users/drastone`, {
+        method: "DELETE",
+      })
+      if (resp.ok) {
+        cargarTareas()
+        console.log("se borro todo")
+      } else {
+        console.log("no se borro nada")
+      }
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
+
+  const crearUsuario = async () => {
+    try {
+      const resp = await fetch("https://playground.4geeks.com/todo/users/drastone", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" }
+      })
+      if (resp.status == 201) {
+        cargarTareas()
+      }
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
+
   useEffect(() => {
     cargarTareas();
   }, []);
-
-
-  
-const borrar = async (id) => {
-		// console.log(id)
-		try {
-			const resp = await fetch("https://playground.4geeks.com/todo/todos/" + id, {
-				method: "DELETE",
-				headers: { "Content-Type": "application/json" }
-			})
-
-			if (resp.status == 204) {
-				cargarTareas()
-				return true
-			}
-		} catch (error) {
-			console.log(error)
-			return false
-		}
-	}
-
-  const crearUsuario = async () => {
-		try {
-			const resp = await fetch("https://playground.4geeks.com/todo/users/drastone", {
-				method: "POST",
-				headers: { "Content-Type": "application/json" }
-			})
-			if (resp.status == 201) {
-				cargarTareas()
-			}
-		} catch (error) {
-			console.log(error)
-			return false
-		}
-	}
 
 
   return (
     <div className="container my-5 background col-4">
       <h1 className="text-center">Tasks</h1>
       <div>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="What's needs to be done?"
-          value={tareaNueva}
-          onChange={(event) => {
-            setTareaNueva(event.target.value);
-          }}
+        <input type="text" className="form-control" placeholder="What's needs to be done?" value={tareaNueva} onChange={(event) => {
+          setTareaNueva(event.target.value);
+        }}
+
           onKeyUp={async (event) => {
             if (event.key === "Enter") {
               const url = "https://playground.4geeks.com/todo/todos/drastone";
@@ -88,33 +105,24 @@ const borrar = async (id) => {
                 }),
               });
               if (resp.ok) {
-                setTareaNueva(""); 
-                cargarTareas(); 
+                setTareaNueva("");
+                cargarTareas();
               }
             }
           }}
         />
 
-<ul>
-  {tareas.length === 0 ? (
-    <li>No hay tareas, añadir tareas</li>
-  ) : (
-    tareas.map((item, index) => (
-      <li key={index}>
-        {item.label}
-        <i
-          className="fa-solid fa-trash icono-oculto"
-          onClick={() => {
-            borrar(item.id); 
-          }}
-        ></i>
-      </li>
-    ))
-  )}
-</ul>
-
+        <ul>{tareas.length === 0 ? (
+          <li>No hay tareas, añadir tareas</li>
+        ) : (tareas.map((item, index) => (
+          <li key={index}>{item.label}<i className="fa-solid fa-trash icono-oculto" onClick={() => { borrar(item.id); }}></i>
+          </li>
+        ))
+        )}
+        </ul>
         <span>{tareas.length} Items left</span>
       </div>
+      <button type="button" onClick= { () => deleteUser()}>Eliminar Todo</button>
     </div>
   );
 };
